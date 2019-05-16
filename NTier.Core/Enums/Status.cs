bc71f23508a5930c271135febe3b11da4f8955ce
baranbaskazanci <baranbaskazanci@gmail.com>
2019-05-16 12:15:34 +0300

    Core katmanına Status enum'ı eklendi

---
+++ NTier.Core/Enums/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Core.Enums
+{
+    public enum Status
+    {
+        None = 0,
+        Active = 1,
+        Deleted = 2,
+        Passive= 4
+    }
+}
